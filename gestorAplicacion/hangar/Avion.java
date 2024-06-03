490e1c230f6039d36a90ad292efbddfbd883cffb
Valeryfer <159395606+Valeryfer@users.noreply.github.com>
2024-06-02 20:40:32 -0500

    Actualización hangar

--- gestorAplicacion/hangar/Avion.java
+++ gestorAplicacion/hangar/Avion.java
@@ -11,3 +11,38 @@ public class Avion extends Aeronave {
 		super(nombre, aerolinea);
 		this.setSILLASECONOMICAS(new Silla[NUM_SILLAS_ECONOMICAS]);
 		this.setSILLASEJECUTIVAS(new Silla[NUM_SILLAS_EJECUTIVAS]);
+
+	// LA VARIABLE UBICACION VA CAMBIANDO SU VALOR SEGUN LOS SIGUIENTES PROCESOS, SE
+		// USA PARA LA ASIGNACION DEL ATRIBUTO UBICACION DE LAS SILLAS.
+		Ubicacion ubicacion = null;
+
+		// EL SIGUIENTE PROCESO CREA Y AGREGA SILLAS A LA LISTA DE SILLAS EJECUTIVAS QUE
+		// POSEE LA CLASE AVION("HEREDA LA LISTA DE AERONAVE")
+		// NOTA: LAS SILLAS DE TIPO EJECUTIVA SE REPARTEN EN GRUPOS DE 4 EN FILA
+		// SEPARADAS POR UN PASILLO.(POR TANTO NO HAY UBICACION CENTRAL)
+		for (int numPosicion = 0; numPosicion < NUM_SILLAS_EJECUTIVAS; numPosicion++) {
+			if (numPosicion % 4 == 0 || numPosicion % 4 == 3) {
+				ubicacion = Ubicacion.VENTANA;
+			} else {
+				ubicacion = Ubicacion.PASILLO;
+			}
+
+			this.getSILLASEJECUTIVAS()[numPosicion] = new Silla(Clase.EJECUTIVA, numPosicion, ubicacion);
+		}
+
+		// EL SIGUIENTE PROCESO CREA Y AGREGA SILLAS A LA LISTA DE SILLAS ECONOMICAS QUE
+		// POSEE LA CLASE AVION("HEREDA LA LISTA DE AERONAVE")
+		// NOTA: LAS SILLAS DE TIPO ECONOMICA SE REPARTEN EN GRUPOS DE 6 EN FILA
+		// SEPARADAS POR UN PASILLO
+		for (int numPosicion = 0; numPosicion < NUM_SILLAS_ECONOMICAS; numPosicion++) {
+			if (numPosicion % 6 == 0 || numPosicion % 6 == 5) {
+				ubicacion = Ubicacion.VENTANA;
+			} else if (numPosicion % 6 == 1 || numPosicion % 6 == 4) {
+				ubicacion = Ubicacion.CENTRAL;
+			} else if (numPosicion % 6 == 2 || numPosicion % 6 == 3) {
+				ubicacion = Ubicacion.PASILLO;
+			}
+			this.getSILLASECONOMICAS()[numPosicion] = new Silla(Clase.ECONOMICA, numPosicion, ubicacion);
+		}
+	}
+}
