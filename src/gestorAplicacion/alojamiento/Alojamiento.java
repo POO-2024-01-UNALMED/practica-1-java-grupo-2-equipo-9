189c8cfdde6815084d5d41b83c75eaec9418b8ad
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-08-14 09:25:10 -0500

    Importaciones alojamiento

--- src/gestorAplicacion/alojamiento/Alojamiento.java
+++ src/gestorAplicacion/alojamiento/Alojamiento.java
@@ -1,6 +1,8 @@
 // Autores: Ricardo Fuentes, Valery Fernandez, Juan Luis Sucerquia, Mariana Sanchez, José Forero
 package gestorAplicacion.alojamiento;
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class Alojamiento {
+public class Alojamiento implements Serializable{
     
 }
